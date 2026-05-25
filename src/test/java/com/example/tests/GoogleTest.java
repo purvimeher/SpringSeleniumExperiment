f692c53cb0aa73b84cd06507984f0a10045f3ea5
meherm <meherpurvi2815@gmail.com>
2026-05-25 11:48:54 +0100

    added app properties

--- src/test/java/com/example/tests/GoogleTest.java
+++ src/test/java/com/example/tests/GoogleTest.java
@@ -4,6 +4,7 @@ import com.example.SpringSeleniumApplication;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = SpringSeleniumApplication.class)
@@ -12,9 +13,12 @@ class GoogleTest {
     @Autowired
     private WebDriver driver;
 
+    @Value("${app.base.url}")
+    private String baseUrl;
+
     @Test
     void openGoogle() {
-        driver.get("https://www.google.com");
+        driver.get(baseUrl);
         System.out.println(driver.getTitle());
     }
 }
